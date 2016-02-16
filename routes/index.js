f34e053d6b0b5ec7981fddb63e9d7fe6df8e9d84
Kateryna Bilokhvost <therry26@gmail.com>
2016-02-16 00:33:40 -0500

    schema is done

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,22 @@ router.get('/', function(req, res, next) {
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact Me' });
 });
-
 
 module.exports = router;
